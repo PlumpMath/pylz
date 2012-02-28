eb463ba02e92f21b2a9274112ab78721168feaca
PLR <plredmond@gmail.com>
2012-02-28 13:30:32 -0500

    style

--- plz/ints.py
+++ plz/ints.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+
+
+#stdlib
 from math import log, ceil
+
+
+###############################################################################
 
 
 def bitwidth(val):
     return ceil(log(val + 1, 2))
+
 
 assert bitwidth(0b0) == 0
 assert bitwidth(0b1) == 1
@@ -15,8 +23,12 @@ assert bitwidth(0b110100101) == 9
 assert bitwidth(0b1010110100) == 10
 
 
+###############################################################################
+
+
 def bytewidth(val):
     return ceil(bitwidth(val) / 8)
+
 
 assert bytewidth(0b0) == 0
 assert bytewidth(0b1) == 1
@@ -27,6 +39,9 @@ assert bytewidth(0b10110111) == 1
 assert bytewidth(0b101101010) == 2
 assert bytewidth(0b1010111010000010) == 2
 assert bytewidth(0b1010111010001000010) == 3
+
+
+###############################################################################
 
 
 def tobytes(val, length=None):
@@ -41,6 +56,7 @@ def tobytes(val, length=None):
         w = length
     return bytes(255 & (val >> 8 * i) for i in reversed(range(w)))
 
+
 assert tobytes(0) == b''
 assert tobytes(0, 2) == b'\x00\x00'
 assert tobytes(97   ) == b'a'
@@ -50,12 +66,16 @@ assert tobytes(97, 3) == b'\x00\x00a'
 assert tobytes(495891539314) == b'super'
 
 
+###############################################################################
+
+
 def frombytes(byt):
     '''Convert bytes to the corresponding integer.'''
     n = 0
     for i, b in enumerate(reversed(list(byt))):
         n |= b << 8 * i
     return n
+
 
 assert frombytes(b'') == 0
 assert frombytes(b'\x00') == 0
@@ -64,3 +84,7 @@ assert frombytes(b'\x02') == 2
 assert frombytes(b'\x02\x80') == 640
 assert frombytes(b'\x00\x02\x80') == 640
 assert frombytes(tobytes(987654321)) == 987654321
+
+
+###############################################################################
+
