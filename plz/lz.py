68a5f607a1a2819df395b184b01cce39e1319d4e
PLR <plredmond@gmail.com>
2012-03-02 17:08:23 -0500

    cleaned up encoder; encoder docstring acknowledges first and last block special behavior; started decoder [currently unfinished]

--- plz/lz.py
+++ plz/lz.py
@@ -19,38 +19,43 @@ Lempel-Ziv (LZ77 ???)
 
 @util.coroutine
 def encode(nxt, quiet=False):
-    '''Produce [pointer][newbyte] bytes according to LempelZiv.
-    
-    Consume single bytes.
+    '''Compress a stream of bytes according to LempelZiv.
+
+    Consume single bytes. Produce pointer-newbyte bytes.
     
     When finished:
+      Send buffer as a lone prefix. (If any leftover)
       Print a message to stderr. (Set quiet to True to disable)
 
+    Note1: The first block has no pointer.
+    Note2: The last block may have no newbyte.
+
     '''
-    blocknum = 0
     table = {}
-    buf = b''
+    blocknum = 0
+    block = b''
     try:
         while True:
             # get a byte
             byte = yield
             if len(byte) != 1:
-                raise ValueError('lz.encoder: got {}'.format(byte))
-            buf += byte
-            # check if it makes the current buffer new
-            if buf not in table:
-                # remember the new block
-                blocknum += 1
-                table[buf] = blocknum
-                # encode the new block
-                known, new = buf[:-1], buf[-1:]
-                pfxptr = ints.tobytes(table[known] if len(known) else blocknum,
+                raise ValueError('lz.encoder: got {} bytes in {}'.
+                                 format(len(byte), byte))
+            block += byte
+            # check if the block is new; if so, compress it
+            if block not in table:
+                table[block] = blocknum
+                known, new = block[:-1], block[-1:]
+                pfxptr = ints.tobytes(table[known] if known else blocknum,
                                       length=ints.bytewidth(blocknum))
-                # send encoded bytes
                 nxt.send(pfxptr + new)
                 # start over
-                buf = b''
+                blocknum += 1
+                block = b''
     finally:
+        if block:
+            nxt.send(ints.tobytes(table[block],
+                                  length=ints.bytewidth(blocknum)))
         if not quiet:
             print('lz.encoder: {} blocks done'.format(blocknum),
                   file=sys.stderr)
@@ -58,3 +63,58 @@ def encode(nxt, quiet=False):
 
 ###############################################################################
 
+
+@util.coroutine
+def decode(nxt, quiet=False):
+    '''Decompress a stream of bytes compressed with "lz.encode".
+
+    Consume single bytes. Produce decompressed bytes.
+    
+    When finished:
+#      Send remaining uncompressed buffer.
+      Print a message to stderr. (Set quiet to True to disable)
+
+    '''
+    table = {}
+    blocknum = 0
+    block = b''
+    try:
+        while True:
+            # get a byte
+            byte = yield
+            if len(byte) != 1:
+                raise ValueError('lz.decoder: got {} bytes in {}'.
+                                 format(len(byte), byte))
+            block += byte
+            # check if the block is complete; if so decompress it
+            width = ints.bytewidth(blocknum) + 1
+            if len(block) == width:
+                # split into prefix and new byte
+                pfxptr, new = block[:-1], block[-1:]
+                # expand it
+                block = pfxptr and table[ints.frombytes(pfxptr)]
+                pfx = ... if pfxptr in table else ...
+                table[blocknum] = (pfxptr and table[pfxptr)]) + new
+                if pfxptr != blocknum:
+                    # lookup prefix in table
+                # remember it
+                table[pfxptr] = new
+                pints.frombytes(pfxptr)
+                table[] = ....
+                table[block] = blocknum
+                pfxptr = ints.tobytes(table[known] if known else blocknum,
+                                      length=ints.bytewidth(blocknum))
+                nxt.send(pfxptr + new)
+                # start over
+                blocknum += 1
+                block = b''
+    finally:
+        if block:
+            nxt.send(ints.tobytes(table[block],
+                                  length=ints.bytewidth(blocknum)))
+        if not quiet:
+            print('lz.encoder: {} blocks done'.format(blocknum),
+                  file=sys.stderr)
+
+
+###############################################################################
